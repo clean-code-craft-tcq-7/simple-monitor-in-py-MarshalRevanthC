686fb42c3653da00929caa5a5d3581466ebbb97c
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2022-12-13 17:57:54 +0530

    Update Temperature_check.py
    Corrected IF statement comparison

--- Temperature_check.py
+++ Temperature_check.py
@@ -2,7 +2,7 @@ TEMP_MIN = 0
 TEMP_MAX = 45
 
 def temp_limit(temp):
-    if temp < TEMP_MIN or temp > TEMP_MIN:
+    if temp < TEMP_MIN or temp > TEMP_MAX:
         print('Temperature is out of range!')
         return False
     return True
