381c2f4016e927b8767d9e2ede8393e77caa4ec9
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2022-12-14 12:07:32 +0530

    Update and rename Temperature_check.py to temperature_limits.py

--- temperature_limits.py
+++ temperature_limits.py
@@ -1,8 +1,9 @@
 TEMP_MIN = 0
 TEMP_MAX = 45
 
-def temp_limit(temp):
+def temp_is_ok(temp):
     if temp < TEMP_MIN or temp > TEMP_MAX:
         print('Temperature is out of range!')
         return False
     return True
+
