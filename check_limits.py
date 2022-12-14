ded4e6a4aa2c67ef307443e9926d993bf27b062a
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2022-12-14 11:07:12 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -15,4 +15,3 @@ def battery_is_ok(temp,soc,charge):
 if __name__ == '__main__':
     assert (battery_is_ok(25, 70, 0.7) is True)
     assert (battery_is_ok(50, 85, 0) is False)
-    assert (battery_is_ok(100, -1, 0.999) is False)
