f59b24477c5a1154f111240aad1e765f7f6ab1d8
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2022-12-14 11:52:02 +0530

    Update charge_rate.py

--- charge_rate.py
+++ charge_rate.py
@@ -1,8 +1,7 @@
 charge_rate_max = 0.8
-charge_rate_min = 0.2
 
 def charge_rate_limit(charge):
-   if charge < charge_rate_min or charge > charge_rate_max:
+   if charge > charge_rate_max:
         print('Charge rate is out of range!')
         return False
    return True
